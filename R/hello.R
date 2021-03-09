2b6004707051b36caf7867afcf62bdaba033d685
Rohan Alexander <RohanAlexander@users.noreply.github.com>
2021-03-09 15:02:49 -0500

    Add readme

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -16,3 +16,13 @@
 hello <- function() {
   print("Hello, world!")
 }
+
+use_git()
+use_github()
+
+library(devtools)
+library(roxygen2)
+library(usethis)
+
+library(available)
+available("ggplot2")
